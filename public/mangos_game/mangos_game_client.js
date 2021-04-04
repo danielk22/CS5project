7a0858416f05280e75ea05e500fa857cb9d586d5
Daniel Kurlander <danielk22@lakesideschool.org>
2021-04-03 21:31:35 -0700

    I've made many changes! The game is almost functional!

--- public/mangos_game/mangos_game_client.js
+++ public/mangos_game/mangos_game_client.js
@@ -1,20 +1,74 @@
 var socket = io();
 var gameID = sessionStorage.getItem('gameID');
 var username = sessionStorage.getItem('username');
+var isJudge;
 
 socket.emit('serverClientRedirected', {gameID: gameID, username: username});
+changeScreenTo('pageIntro');
 
 function sendToHost(gameID, event, message) {
     socket.emit('serverSendToHost', {gameID: gameID, event: event, hostMessage: message})
 }
 
 
-//EVENTS BELOW ARE FOR DEMONSTRATION PURPOSES ONLY
-socket.on('clientWelcome', function(message) { // message is string to display
-    document.getElementById('gameStatus').textContent = message;
-    sendToHost(gameID, 'hostReceivedWelcome', `Player ${username} appreciates your welcome!`);
-});
-
 socket.on('clientPersonalMessage', function(message) { // message is string to display
     document.getElementById('personalMessage').textContent = message;
 });
+
+socket.on('clientDeclareJudge', function(message) { //message is judge username
+    changeScreenTo('inRound');
+    isJudge = (message == username);
+    var role;
+    if (isJudge) {
+        role = 'judge';
+        document.getElementById('hand').textContent = '';
+    }
+    else {
+        role = 'player';
+    }
+    document.getElementById('role').textContent = role;
+});
+
+socket.on('clientRecieveGreenCard', function(message) { //message is the green card
+    document.getElementById('greenCard').innerHTML = `${message.title} <br> ${message.descrip}`;
+});
+
+socket.on('clientUpdateHand', function(message) { //message is the player's hand
+    str = '';
+    var hand = message;
+    if (!isJudge) {
+       displayHand(hand);
+    }
+});
+
+socket.on('clientCardsToJudge', function(message) { //message is an array of cards to judge
+    var cards = message;
+    displayHand(cards);
+});
+
+
+function displayHand(hand) {
+    str = '';
+    for (var i = 0; i < hand.length; i++) {
+        str += `<li><a onclick = "chooseCard(${i})"> ${hand[i].title} <br> ${hand[i].descrip} </a></li>`; 
+    }
+    document.getElementById('hand').innerHTML = str;
+}
+
+
+function chooseCard(cardIndex) {
+    if (!isJudge) {
+        sendToHost(gameID, 'hostReceiveChosenCard', {username: username, selectedCardIndex: cardIndex});
+        changeScreenTo('postSelect');
+    }
+    else {
+        sendToHost(gameID, 'hostRecieveWinningCard', cardIndex);
+    }
+}
+
+function changeScreenTo(screen) {
+    const screens = ['pageIntro', 'postSelect', 'inRound'];
+    for (var i = 0; i < screens.length; i++) {
+        document.getElementById(screens[i]).style.display = (screens[i] === screen ? 'block' : 'none');
+    }
+}
--- public/mangos_game/mangos_game_host.js
+++ public/mangos_game/mangos_game_host.js
@@ -1,10 +1,19 @@
 var socket = io();
 var players;
 var gameID;
+var redCards;
+var greenCards;
+var currentRed = 0;
+var currentGreen = 0;
+var judgeIndex;
+const cardsPerHand = 7;
+var selectedCards = [];
+
+
 
 const gameDescriptor = {
     minPlayers: 2,
-    maxPlayers: 3,
+    maxPlayers: 10,
     clientURL: 'mangos_game/mangos_game_client.html'
 };
 
@@ -21,27 +30,55 @@ socket.on('hostRegisterNewGame', function(message) {
 socket.on('hostNewPlayerList', function(message) {
     console.log('got a hostNewPlayerList event');
     players = message;
-    var lastPlayer = players[players.length - 1];    
-    document.getElementById('players').innerHTML += `<tr><td>${lastPlayer.username}</td><td>0</td></tr>`; //Hard-code everyone's initial score to 0
-    
+    updateScore(); //This displays current player list and score (which is initially set to 0)
 });
 
 function storeRed() {
     var iframeDoc = document.getElementById('redCards').contentWindow.document;
     var redCardsStr =  iframeDoc.getElementById('cardList').innerHTML;
+    redCards = retrieveCardInfo(redCardsStr);
 }
 
 function storeGreen() {
     var iframeDoc = document.getElementById('greenCards').contentWindow.document;
     var greenCardsStr = iframeDoc.getElementById('cardList').innerHTML;
+    greenCards = retrieveCardInfo(greenCardsStr);
 }
 
 function retrieveCardInfo(stringList) {
     var wordsByLine = stringList.split('\n');
+    var cardsArray = [];
     for (var i = 0; i < wordsByLine.length; i++) {
-        
+        var titleStart = wordsByLine[i].indexOf('<b>') + 3;
+        var titleEnd = wordsByLine[i].indexOf('</b>');
+        if (titleStart >= 0 && titleEnd >= titleStart) {
+            var strTitle = wordsByLine[i].slice(titleStart, titleEnd);
+            var descripStart = titleEnd + 7; //distance between end of title and start of description
+            var descripEnd = wordsByLine[i].indexOf('<u>') - 1;
+            if (descripEnd >= 0) {
+                var strDescrip = wordsByLine[i].slice(descripStart, descripEnd);
+                var setStart = descripEnd + 4;
+                var setEnd = wordsByLine[i].indexOf('</u>');
+                if (setEnd >= 0) {
+                    var strSet = wordsByLine[i].slice(setStart, setEnd);
+                    cardsArray.push({title: strTitle, descrip: strDescrip, set: strSet});
+                }
+            }
+        }
+    }
+    return cardsArray;
+}
+
+//Got this shuffle algorithm from wikipedia! (Second example for Fisher-Yates modern algorithm)
+function shuffleCards(cards) {
+    for (var i = 0; i < cards.length - 1; i++) {
+        var j = Math.floor((Math.random() * (cards.length - i)) + i);
+        toSwap = cards[i];
+        cards[i] = cards[j];
+        cards[j] = toSwap;
     }
 }
+
 
 function startGame() {
     console.log('The start game button has been pressed!');
@@ -58,20 +95,98 @@ function sendToPlayer(gameID, username, event, message) {
 
 socket.on('hostGameStart', function () {
     document.getElementById('startMessage').textContent = 'The game has started!';
+    shuffleCards(redCards);
+    shuffleCards(greenCards);
     for (var i = 0; i < players.length; i++) {
-        players[i].score = 0; //set every player's score to 0 at the start of the game.
+        players[i].score = 0; //set every player's score to 0 at the start of the game, set their hand to empty.
+        players[i].hand = [];
+        players[i].selectedCardIndex = -1; //set every player to not have selected a card
     }
-    sendToAllPlayers(gameID, 'clientWelcome', 'Welcome to the game! - A message from the host');
+    judgeIndex = Math.floor(Math.random() * players.length);
+    doNextRound();
 });
 
-//EVENT BELOW FOR DEMONSTRATION PURPOSES ONLY
-socket.on('hostReceivedWelcome', function(message) { //message is the string to display
-    document.getElementById("welcomeList").innerHTML += `<li> ${message} </li>`;
-    //send an individualized welcome to each player
+function doNextRound() {
+    changeScreenTo('inRound')
+    document.getElementById('selectedCardNum').textContent = '0';
+    document.getElementById('judgeDisplay').textContent = `This round's judge is: ${players[judgeIndex].username}`;
+    document.getElementById('greenCardDisplay').innerHTML = `${greenCards[currentGreen].title} <br> ${greenCards[currentGreen].descrip}`;
+    sendToAllPlayers(gameID, 'clientDeclareJudge', players[judgeIndex].username);
+    sendToAllPlayers(gameID, 'clientRecieveGreenCard', greenCards[currentGreen]);
+    for (var i = 0; i < players.length; i++) { //Fill each hand 
+            fillHand(players[i].hand);
+            sendToPlayer(gameID, players[i].username, 'clientUpdateHand', players[i].hand);
+    }
+}
+
+socket.on('hostReceiveChosenCard', function(message) { //message has a username and selectedCardIndex
+    var player = getPlayerByName(message.username);
+    player.selectedCardIndex = message.selectedCardIndex;
+    player.hand[player.selectedCardIndex].username = message.username;
+    selectedCards.push(player.hand[player.selectedCardIndex]);
+    document.getElementById('selectedCardNum').textContent = `${selectedCards.length}`;
+    if (selectedCards.length === players.length - 1) {
+        changeScreenTo('inJudging');
+        str = '';
+        for (var i = 0; i < selectedCards.length; i++) {
+            str += `<li> ${selectedCards[i].title} <br> ${selectedCards[i].descrip} </li>`;
+        }
+        document.getElementById('chosenCards').innerHTML = str;
+        sendToPlayer(gameID, players[judgeIndex].username, 'clientCardsToJudge', selectedCards);
+    }
+});
+
+socket.on('hostRecieveWinningCard', function(message) { //message is the winning card index
+    changeScreenTo('endRound');
+    var selectedIndex = message;
+    document.getElementById('winner').innerHTML = `${selectedCards[selectedIndex].username} won with the card 
+                                                   ${selectedCards[selectedIndex].title} <br> 
+                                                   ${selectedCards[selectedIndex].descrip}`;
+    getPlayerByName(selectedCards[selectedIndex].username).score ++;
+    updateScore();
+    judgeIndex = (judgeIndex + 1) % players.length;
+    currentGreen++;
+    removeUsedCards();
+    doNextRound();
+});
+
+function removeUsedCards() {
     for (var i = 0; i < players.length; i++) {
-        sendToPlayer(gameID, players[i].username, 'clientPersonalMessage', 
-                    `Hello ${players[i].username}! The server sent this message only to you.`);
-    }   
-});
+        if (players[i].selectedCardIndex != -1) {
+            players[i].hand.splice(players[i].selectedCardIndex, 1);
+            players[i].selectedCardIndex = -1;
+        }
+    }
+    selectedCards = [];
+}
 
+function updateScore() {
+    str = '';
+    for (var i = 0; i < players.length; i++) {
+        var score = (players[i].score == undefined ? '0' : players[i].score);
+        str += `<tr><td>${players[i].username}</td><td>${score}</td></tr>`;
+    }
+    document.getElementById('players').innerHTML = str;
+}
 
+function fillHand(hand) {
+    while (hand.length < cardsPerHand) {
+        hand.push(redCards[currentRed++]);
+    }
+}
+
+function getPlayerByName(name) {
+    for (var i = 0; i < players.length; i++) {
+        if (name === players[i].username) {
+            return players[i];
+        }
+    }
+    return undefined;
+}
+
+function changeScreenTo(screen) {
+    const screens = ['pageIntro', 'inRound', 'inJudging', 'endRound'];
+    for (var i = 0; i < screens.length; i++) {
+        document.getElementById(screens[i]).style.display = (screens[i] === screen ? 'block' : 'none');
+    }
+}
